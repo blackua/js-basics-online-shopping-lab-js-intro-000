095a0558aac385b20028a7ee7d7de990cda3828b
Norathat Jongjitnant <norathat@gmail.com>
2020-09-24 03:32:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -56,7 +56,12 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
+  var message = ""
   if (!cardNumber) {
     return 'Sorry, we don\'t have a credit card on file for you.'
+  } else {
+    message = `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
+    cart = []
+    return message
   }
 }
